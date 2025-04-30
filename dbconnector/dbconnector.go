5010c1a7bf68188ecd06d9259fa3702570e901ae
Sumir Vats <sumirvats@gmail.com>
2025-04-30 02:19:04 +0000

    refactor dbconnection

---
+++ dbconnector/dbconnector.go
@@ -0,0 +1,40 @@
+package dbconnector
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func ConnectPostgres() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
--- main.go
+++ main.go
@@ -1,38 +1,9 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"os"
-	"strconv"
-
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+import(
+	"github.com/SumirVats2003/workout-api/dbconnector"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err == nil {
-		host := os.Getenv("DB_HOST")
-		port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
-		user := os.Getenv("DB_USER")
-		password := os.Getenv("DB_PASSWORD")
-		dbname := os.Getenv("DB_NAME")
-		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-			"password=%s dbname=%s sslmode=disable",
-			host, port, user, password, dbname)
-
-		db, err := sql.Open("postgres", psqlInfo)
-		if err != nil {
-			panic(err)
-		}
-		defer db.Close()
-
-		err = db.Ping()
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("Successfully connected!")
-	}
+	dbconnector.ConnectPostgres()
 }
