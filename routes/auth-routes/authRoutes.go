8f148bed7f3b9a0bfe29365b73f979660f4527cb
Sumir Vats <sumirvats@gmail.com>
2025-05-01 16:26:13 +0000

    fix: routing structure finalisation

--- routes/auth-routes/authRoutes.go
+++ routes/auth-routes/authRoutes.go
@@ -8,19 +8,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func AuthRoutes(db *sql.DB) {
-	muxRouter := mux.NewRouter()
+func loginUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome to login route")
+}
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome to register route")
+}
+
+func AuthRoutes(muxRouter *mux.Router, db *sql.DB) {
 	db.Ping() // INFO: can be removed when more sophisticated db calls are implemented
 
 	authRouter := muxRouter.PathPrefix("/auth").Subrouter()
-
-	authRouter.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "welcome to login route")
-	}).Methods("POST")
-
-	authRouter.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "welcome to register route")
-	}).Methods("POST")
-
-	http.ListenAndServe(":3000", nil)
+	authRouter.HandleFunc("/login", loginUser).Methods("POST")
+	authRouter.HandleFunc("/register", registerUser).Methods("POST")
 }
--- routes/router.go
+++ routes/router.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/SumirVats2003/workout-api/dbconnector"
-	authroutes "github.com/SumirVats2003/workout-api/routes/auth-routes"
+	authRoutes "github.com/SumirVats2003/workout-api/routes/auth-routes"
+	"github.com/gorilla/mux"
 )
 
 func InitRoutes() {
+	router := mux.NewRouter()
 	db := dbconnector.OpenDBConnection()
-	authroutes.AuthRoutes(db)
+	authRoutes.AuthRoutes(router, db)
+	http.ListenAndServe(":3000", router)
 }
