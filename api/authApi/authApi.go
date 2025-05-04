357864482efd6aaf8d615f75e55ae015d24ad008
Sumir Vats <sumirvats@gmail.com>
2025-05-04 05:06:37 +0000

    added json body decoding for login api

--- api/authApi/authApi.go
+++ api/authApi/authApi.go
@@ -1,12 +1,39 @@
 package authapi
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/SumirVats2003/workout-api/models"
 )
 
+type credentials struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to login route")
+	var payload credentials
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user := models.User{
+		Name:   payload.Name,
+		Email:  payload.Email,
+		UserId: "7898",
+	}
+
+	password := payload.Password
+	fmt.Println(password)
+
+	fmt.Fprintf(w, "user : %+v", user)
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
