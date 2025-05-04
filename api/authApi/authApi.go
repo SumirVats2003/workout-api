71e967c7c8744293781a9e3a68a8384c1592becb
Sumir Vats <sumirvats@gmail.com>
2025-05-04 15:51:44 +0000

    refactor route-api coupling

--- api/authApi/authApi.go
+++ api/authApi/authApi.go
@@ -1,41 +1,18 @@
 package authapi
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"net/http"
 
 	"github.com/SumirVats2003/workout-api/models"
 )
 
-type credentials struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	var payload credentials
-
-	err := json.NewDecoder(r.Body).Decode(&payload)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	user := models.User{
-		Name:   payload.Name,
-		Email:  payload.Email,
-		UserId: "7898",
-	}
-
-	password := payload.Password
+func Login(db *sql.DB, user models.User, password string) {
+	fmt.Println(user)
 	fmt.Println(password)
-
-	fmt.Fprintf(w, "user : %+v", user)
 }
 
-func Register(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to register route")
+func Register(db *sql.DB, user models.User, password string) {
+	fmt.Println(user)
+	fmt.Println(password)
 }
--- routes/auth-routes/authRoutes.go
+++ routes/auth-routes/authRoutes.go
@@ -2,14 +2,60 @@ package authroutes
 
 import (
 	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
 	authapi "github.com/SumirVats2003/workout-api/api/authApi"
+	"github.com/SumirVats2003/workout-api/models"
 	"github.com/gorilla/mux"
 )
 
-func AuthRoutes(muxRouter *mux.Router, db *sql.DB) {
-	db.Ping() // INFO: can be removed when more sophisticated db calls are implemented
+type credentials struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
+func parseJSON(payload *credentials, w http.ResponseWriter, r *http.Request) (models.User, string, error) {
+	err := json.NewDecoder(r.Body).Decode(payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return models.User{}, "", err
+	}
+	user := models.User{
+		Name:  payload.Name,
+		Email: payload.Email,
+	}
+	password := payload.Password
+
+	return user, password, nil
+}
+
+func AuthRoutes(muxRouter *mux.Router, db *sql.DB) {
 	authRouter := muxRouter.PathPrefix("/auth").Subrouter()
-	authRouter.HandleFunc("/login", authapi.Login).Methods("POST")
-	authRouter.HandleFunc("/register", authapi.Register).Methods("POST")
+
+	authRouter.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		var payload credentials
+		user, password, err := parseJSON(&payload, w, r)
+
+		if err != nil {
+			log.Fatal(err.Error())
+			return
+		}
+
+		authapi.Login(db, user, password)
+	}).Methods("POST")
+
+	authRouter.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		var payload credentials
+		user, password, err := parseJSON(&payload, w, r)
+
+		if err != nil {
+			log.Fatal(err.Error())
+			return
+		}
+
+		authapi.Register(db, user, password)
+	}).Methods("POST")
 }
