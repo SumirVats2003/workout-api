48448c628fb3fe7c5ea99d674bfd9494849c207d
Sumir Vats <sumirvats@gmail.com>
2025-05-05 02:56:10 +0000

    added password hashing on registeration

--- api/authApi/authApi.go
+++ api/authApi/authApi.go
@@ -8,7 +8,16 @@ import (
 
 	"github.com/SumirVats2003/workout-api/models"
 	"github.com/SumirVats2003/workout-api/utils"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func hashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return ""
+	}
+	return string(bytes)
+}
 
 func Login(db *sql.DB, email string, password string) {
 	fmt.Println(email)
@@ -17,6 +26,12 @@ func Login(db *sql.DB, email string, password string) {
 
 func Register(db *sql.DB, user models.User, password string) {
 	user.UserId = utils.GenerateUUID()
+	passwordHash := hashPassword(password)
+
+	if passwordHash == "" {
+		log.Fatal("could not hash password")
+		return
+	}
 
 	res, err := db.Exec(
 		"INSERT INTO users (id, created_at, name, email, password) VALUES ($1, $2, $3, $4, $5)",
@@ -24,7 +39,7 @@ func Register(db *sql.DB, user models.User, password string) {
 		time.Now(),
 		user.Name,
 		user.Email,
-		password,
+		passwordHash,
 	)
 
 	if err != nil {
