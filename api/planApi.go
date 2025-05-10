42a66dcb14f785fbca02f3935fda29d58cf7b9d3
Sumir Vats <sumirvats@gmail.com>
2025-05-10 04:07:34 +0000

    added plan api functions

---
+++ api/planApi.go
@@ -0,0 +1,10 @@
+package api
+
+import "database/sql"
+
+func GetPlan(db *sql.DB) {}
+func CreatePlan(db *sql.DB) {}
+func SchedulePlan(db *sql.DB) {}
+func UpdatePlan(db *sql.DB) {}
+func DeletePlan(db *sql.DB) {}
+func GetAllPlans(db *sql.DB) {}
